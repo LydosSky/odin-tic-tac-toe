135d2beb4f874832efdda742483ca1f2953e47a6
Lydos <lydossky@gmail.com>
2024-10-25 15:54:04 +0300

    board creation and horizontal winner

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 const GameBoard = (function () {
-  let gameBoard = [];
+  let gameBoard;
+  let defaultBoardSize = 3;
 
   const getGameBoard = () => gameBoard;
+
   const placePlayerMarkers = (player) => {
     player.displayPlacedMarkers().forEach((marker) => {
       if (gameBoard[marker] === undefined) gameBoard[marker] = player.marker;
     });
   };
 
-  const resetGameBoard = () => (gameBoard = []);
+  const resetGameBoard = () =>
+    (gameBoard = new Array(defaultBoardSize * defaultBoardSize));
+
+  const isGameFinished = (player) => {
+    // Check for Horizontal winning condition
+    for (let i = 0; i < gameBoard.length; i += defaultBoardSize) {
+      if (gameBoard[i] === player.marker)
+        if (gameBoard[i] === gameBoard[i + 1])
+          if (gameBoard[i + 1] === gameBoard[i + 2]) return true;
+    }
+
+    return false;
+  };
+
+  const createBoard = (size = defaultBoardSize) => {
+    gameBoard = new Array(size * size);
+  };
+
+  const setBoardSize = (size) => {
+    defaultBoardSize = size;
+  };
 
   return {
     getGameBoard,
     placePlayerMarkers,
     resetGameBoard,
+    isGameFinished,
+    createBoard,
+    setBoardSize,
   };
 })();
 
